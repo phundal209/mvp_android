8c1c790696580226681ddf6051991888075f8605
Paul Hundal <p_hundal@yahoo.com>
2018-03-12 23:12:57 -0700

    add view pager transitions

--- app/src/main/java/com/example/phundal2091/basicapplication/ui/root/MainActivity.java
+++ app/src/main/java/com/example/phundal2091/basicapplication/ui/root/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.phundal2091.basicapplication.BasicApplication;
 import com.example.phundal2091.basicapplication.R;
@@ -21,6 +23,7 @@ import com.google.android.gms.location.places.GeoDataClient;
 
 import javax.inject.Inject;
 
+import static android.widget.Toast.LENGTH_LONG;
 import static com.example.phundal2091.basicapplication.prefs.PrefManager.LOCATION_KEY;
 import static com.example.phundal2091.basicapplication.wrapper.LocationClient.LOCATION_PERMISSION_TAG;
 
@@ -29,6 +32,10 @@ public class MainActivity extends FragmentActivity {
     private MainActivityComponent component;
     @Inject
     IContentViewPresenter contentViewPresenter;
+    CityGuidePagerAdapter cityGuidePagerAdapter;
+
+    private TextView barPager, bistroPager, cafePager;
+    private ViewPager viewPager;
 
     public MainActivityComponent component() {
         if (component == null) {
@@ -47,6 +54,10 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        barPager = findViewById(R.id.bar_pager);
+        bistroPager = findViewById(R.id.bistro_pager);
+        cafePager = findViewById(R.id.cafe_pager);
+        viewPager = findViewById(R.id.viewPager);
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(getString(R.string.app_name));
         toolbar.setTitleTextColor(getColor(R.color.white));
@@ -59,9 +70,36 @@ public class MainActivity extends FragmentActivity {
     }
 
     private void bindViewPager() {
-        CityGuidePagerAdapter cityGuidePagerAdapter = new CityGuidePagerAdapter(getSupportFragmentManager());
+        cityGuidePagerAdapter = new CityGuidePagerAdapter(getSupportFragmentManager());
         final ViewPager viewPager = findViewById(R.id.viewPager);
         viewPager.setAdapter(cityGuidePagerAdapter);
+        changePager();
+    }
+
+    private void changePager() {
+        barPager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0);
+                Toast.makeText(MainActivity.this, "On page 0", LENGTH_LONG).show();
+            }
+        });
+
+        bistroPager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+                Toast.makeText(MainActivity.this, "On page 1", LENGTH_LONG).show();
+            }
+        });
+
+        cafePager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(2);
+                Toast.makeText(MainActivity.this, "On page 2", LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
